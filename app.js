9a842929a90952e97b51b048c16a80aae73cc825
Luong Tran <luongductran371@gmail.com>
2025-03-06 16:04:46 -0600

    Added simple form to upload pdf file

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+app.set('json spaces', 5);
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/",(request, response) => {
+    response.send(`
+        <h2>File Upload With <code>NodeJs Express</code></h2>
+        <form action = "/api/upload" enctype="multipart/form-data" method="post">
+            <div>
+                Select File: <input type="file" name="file" multiple="multiple" />
+            </div>
+            <input type="submit" value="Upload"></input>
+        </form>
+    `);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
