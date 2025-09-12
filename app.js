89614807bc2047c8d041bd6efbd39ae825fba79d
Luong Tran <luongductran371@gmail.com>
2025-09-12 18:33:09 +0700

    Getting started again with simple nodejs function

--- app.js
+++ app.js
@@ -1,34 +1,15 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
+const port = 3000;  
 
-app.set("json spaces", 5);
-
-const PORT = process.env.PORT || 3000;
-const fileparser = require("./file-parser");
-
-app.get("/", (request, response) => {
-  response.send(`
-        <h2>File Upload With <code>NodeJs Express</code></h2>
-        <form action = "/api/upload" enctype="multipart/form-data" method="post">
-            <div>
-                Select File: <input type="file" name="file" multiple="multiple" />
-            </div>
-            <input type="submit" value="Upload"></input>
-        </form>
-    `);
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.post("/api/upload", async (request, response) => {
-  await fileparser(request)
-    .then((data) => {
-      response.status(200).json({ message: "Success", data });
-    })
-    .catch((error) => {
-      response.status(400).json({ message: "An error occured", error });
-    });
+app.post("/upload", (req, res) => {
+  res.send("Uploaded");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
 });
