5f42be7e2dfe0aefc7f8530782aeaa69e124b272
Luong Tran <luongductran371@gmail.com>
2025-09-23 17:14:57 +0700

    Added doc parser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const pdf = require("pdf-parse");
+const mammoth = require("mammoth");
 const upload = require("./middleware/uploadMiddleware");
 const resumeParser = require("./services/resumeParser");
 
@@ -9,23 +10,33 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
 app.post("/upload", upload.single("resume"), (req, res) => {
   const dataBuffer = fs.readFileSync(req.file.path);
-  pdf(dataBuffer)
-    .then((data) => {
-      console.log(data.text);
-      const parsedData = resumeParser(data.text);
-      console.log("Parsed Data:", JSON.stringify(parsedData, null, 2));
-      res.send(parsedData);
-    })
-    .catch((err) => {
-      console.error("Error parsing PDF:", err);
-      res.status(500).send("Failed to parse resume.");
-    });
+  // if it's pdf file then parse it using pdf-parse
+  if (req.file.mimetype === "application/pdf") {
+    pdf(dataBuffer)
+      .then((data) => {
+        console.log(data.text);
+        const parsedData = resumeParser(data.text);
+        console.log("Parsed Data:", JSON.stringify(parsedData, null, 2));
+        res.send(parsedData);
+      })
+      .catch((err) => {
+        console.error("Error parsing PDF:", err);
+        res.status(500).send("Failed to parse resume.");
+      });
+  } else if (req.file.mimetype === "application/msword" || req.file.mimetype === "application/vnd.openxmlformats-officedocument.wordprocessingml.document") {
+    mammoth
+      .extractRawText({ buffer: dataBuffer })
+      .then((result) => {
+        console.log(result.value);
+        const parsedData = resumeParser(result.value);
+        console.log("Parsed Data:", JSON.stringify(parsedData, null, 2));
+        res.send(parsedData);
+      })
+      .catch((err) => {
+        console.error("Error parsing DOC:", err)});
+  }
 });
 
 app.listen(port, () => {
